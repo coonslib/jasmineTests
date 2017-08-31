46357623cd724c7fa2f74d25fa692fd694f3e702
n0284106 <n0284106@VDDR35X-G28H019.lm.lmig.com>
2017-08-31 11:34:32 -0700

    added source and tests

---
+++ src/mocks.js
@@ -0,0 +1,21 @@
+var MyType = require("./Functions.js");
+
+// Call prototype_a
+var f = new MyType();
+parmA = 3;
+Proto_A = f.prototype_a(parmA);
+console.log("");
+
+// Call prototype_b
+parmB = 5;
+Proto_B = f.prototype_b(parmB);
+console.log("");
+
+// Call prototype_c
+parmC = 9;
+Proto_C = f.prototype_c(parmC);
+console.log("");
+
+// Call prototype_d
+parmD = 7;
+Proto_D = f.prototype_d(parmD);
